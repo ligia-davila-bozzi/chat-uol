95510f612cd7651f843c878122b2138f393525c9
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-05 12:59:23 -0300

    Showing correct formatted messages, refreshing every 3 seconds and scrolling automatically upon new messages

--- scripts/script.js
+++ scripts/script.js
@@ -12,14 +12,39 @@ function showMessages(response){
     main.innerHTML = "";
     for(let i = 0; i < response.data.length; i++){
 
-        main.innerHTML += `
-        <div class=${response.data[i].type}>
-            <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
-        </div>
-        `
+        if(response.data[i].type === "status"){
+            main.innerHTML += `
+            <div class=${response.data[i].type}>
+                <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span>  ${response.data[i].text}</pre>
+            </div>
+            `
+        }
+        else if(response.data[i].type === "message"){
+            main.innerHTML += `
+            <div class=${response.data[i].type}>
+                <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
+            </div>
+            `
+        }
+        else{
+            main.innerHTML += `
+            <div class=${response.data[i].type}>
+                <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> reservadamente para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
+            </div>
+            `
+        }
+        
         
     }
+
+    scrollToLastMessage(response);
 }
 
+function scrollToLastMessage(response){
+    const div = document.querySelectorAll("div")[response.data.length - 1];
+    div.scrollIntoView();
+}
 
 getMessages();
+setInterval(getMessages, 3000);
+
