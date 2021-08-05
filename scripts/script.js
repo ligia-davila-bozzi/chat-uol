d2d95b8a7c7c42132f531ef7acc8f51ae6a11df7
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-05 14:19:51 -0300

    Enter name available, filter for reserved messages added and maintaining conexion implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
+let username;
 
+function enterRoom(){
+    username = prompt("Qual o seu nome?");
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants", {name: username});
+
+    promise.then(getMessages);
+    promise.catch(validateName);
+    setInterval(getMessages, 3000);
+    setInterval(maintainConexion, 5000);
+}
+
+function validateName(error){
+    if(error.response.status === 400){
+        alert("Usuário já existente!");
+        enterRoom();
+    }
+}
+
+function maintainConexion(){
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status", {name: username});
+}
 
 function getMessages(){
     const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
@@ -27,11 +48,13 @@ function showMessages(response){
             `
         }
         else{
-            main.innerHTML += `
-            <div class=${response.data[i].type}>
-                <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> reservadamente para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
-            </div>
-            `
+            if(isItYou(response.data[i].from) || isItYou(response.data[i].to)){
+                main.innerHTML += `
+                <div class=${response.data[i].type}>
+                    <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> reservadamente para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
+                </div>
+                `
+            }
         }
         
         
@@ -45,6 +68,11 @@ function scrollToLastMessage(response){
     div.scrollIntoView();
 }
 
-getMessages();
-setInterval(getMessages, 3000);
+function isItYou(messageName){
+    if(username === messageName) return true;
+    else return false;
+}
+
+
+enterRoom();
 
