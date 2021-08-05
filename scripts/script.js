fd2873be8672c2d79cd2b7365dec16a48df3862a
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-05 12:29:38 -0300

    Separating messages by type

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+
+
 function getMessages(){
     const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages");
 
@@ -9,16 +11,15 @@ function showMessages(response){
 
     main.innerHTML = "";
     for(let i = 0; i < response.data.length; i++){
+
         main.innerHTML += `
-        <div class="message">
+        <div class=${response.data[i].type}>
             <pre><span class="time">(${response.data[i].time})</span>  <span class="person">${response.data[i].from}</span> para <span class="person">${response.data[i].to}:</span>  ${response.data[i].text}</pre>
         </div>
         `
-
-        main.querySelectorAll("div")[i].style.marginBottom = "4px";
+        
     }
-
-    
 }
+
 
 getMessages();
